0ff4bbb52cccbaf0c5891d53aa593c6a134b21ce
pixis1 <80167203+pixis1@users.noreply.github.com>
2023-06-26 08:48:47 +0000

    Add files via upload

---
+++ pr4/Program.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Reflection.PortableExecutable;
+using System.Text.RegularExpressions;
+
+FileInfo secretFile = new FileInfo(@"C:\Users\npk-s80-1\Documents\pop.txt");
+FileInfo userFile = new FileInfo(@"C:\Users\npk-s80-1\Documents\users.txt");
+
+Console.Write("Что собираетесь сделать?\nc - Создание пользователя\nEnter - Войти в систему\n");
+string a = Console.ReadLine();
+switch (a)
+{
+    case "c":
+        create_user();
+        break;
+    case "":
+        enter_system();
+        break;
+    default:
+        Console.WriteLine("Вы ввели неправильное значение!");
+        break;
+}
+
+
+void create_user()
+{
+    try
+    {
+        string buf0, buf1;
+
+        Console.Write("Логин: ");
+        buf0 = Console.ReadLine();
+        if (!valid_login(buf0)) throw new Exception("Логин не правильный!");
+        
+        Console.Write("Пароль: ");
+        buf1 = Console.ReadLine();
+        if (!valid_password(buf1)) throw new Exception("Пароль не правильный!");
+
+        File.AppendAllText(userFile.FullName, "login:" + buf0 + "\n");
+        File.AppendAllText(userFile.FullName, "password:" + buf1 + "\n");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"{e}");
+    }
+}
+void enter_system()
+{
+    try
+    {
+        File.AppendAllText(userFile.FullName, "");
+        string buf = File.ReadAllText(userFile.FullName);
+        if (buf == "") throw new Exception("Нет пользователей. Создайте.");
+
+        string password, login;
+        Console.WriteLine("Логин: ");
+        login = Console.ReadLine();
+
+        string validPassword = find_login(login);
+        Console.WriteLine("Пароль: ");
+        password = Console.ReadLine();
+        if (password != validPassword) throw new Exception("Пароль неверен. Логин верен.");
+        Console.WriteLine("Вы вошли в систему! Секретные данные: ");
+        Console.WriteLine(File.ReadAllText(secretFile.FullName));
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e);
+    }
+
+
+}
+
+bool valid_password(string str)
+{
+    if (str.Length >= 6 && !Regex.IsMatch(str, @"\p{IsCyrillic}") && !Regex.IsMatch(str, @"\d"))
+    {
+        return true;
+    }
+    else return false;
+}
+bool valid_login(string str)
+{
+    if (!Regex.IsMatch(str, @"\p{IsCyrillic}") && !Regex.IsMatch(str, @"\d"))
+    {
+        return true;
+    }
+    else return false;
+}
+string find_login(string login)
+{
+    string buf = File.ReadAllText(userFile.FullName);
+    int i = buf.IndexOf("login:" + login);
+    if (i == -1) throw new Exception("Неверный логин, такого логина нет.");
+    while (buf[i] != '\n')
+    {
+        i++;
+    }
+    while (buf[i] != ':')
+    {
+        i++;
+    }
+    i++;
+    int a = i;
+    while (buf[i] != '\n')
+    {
+        i++;
+    }
+    i++;
+    string password = buf.Substring(a, i - a - 1);
+    return password;
+}
