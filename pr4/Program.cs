b36bba4fefa3f527efa0cfb41c7db1659686f5f6
pixis1 <80167203+pixis1@users.noreply.github.com>
2023-06-28 01:18:03 -0700

    Add files via upload

--- pr4/Program.cs
+++ pr4/Program.cs
@@ -6,8 +6,7 @@ FileInfo secretFile = new FileInfo(@"C:\Users\npk-s80-1\Documents\pop.txt");
 FileInfo userFile = new FileInfo(@"C:\Users\npk-s80-1\Documents\users.txt");
 
 Console.Write("Что собираетесь сделать?\nc - Создание пользователя\nEnter - Войти в систему\n");
-string a = Console.ReadLine();
-switch (a)
+switch (Console.ReadLine())
 {
     case "c":
         create_user();
@@ -25,7 +24,7 @@ void create_user()
 {
     try
     {
-        string buf0, buf1;
+        string buf0, buf1, buf2, buf3, buf4;
 
         Console.Write("Логин: ");
         buf0 = Console.ReadLine();
@@ -33,10 +32,24 @@ void create_user()
         
         Console.Write("Пароль: ");
         buf1 = Console.ReadLine();
+        buf1 = buf1.ToLower();
         if (!valid_password(buf1)) throw new Exception("Пароль не правильный!");
+
+        Console.Write("ФИО: ");
+        buf2 = Console.ReadLine();
+
+        Console.Write("Город: ");
+        buf3 = Console.ReadLine();
+
+        Console.Write("Номер: ");
+        buf4 = Console.ReadLine();
 
         File.AppendAllText(userFile.FullName, "login:" + buf0 + "\n");
         File.AppendAllText(userFile.FullName, "password:" + buf1 + "\n");
+        File.AppendAllText(userFile.FullName, "nsp:" + buf2 + "\n");
+        File.AppendAllText(userFile.FullName, "city:" + buf3 + "\n");
+        File.AppendAllText(userFile.FullName, "number:" + buf4 + "\n");
+
     }
     catch (Exception e)
     {
@@ -59,8 +72,20 @@ void enter_system()
         Console.WriteLine("Пароль: ");
         password = Console.ReadLine();
         if (password != validPassword) throw new Exception("Пароль неверен. Логин верен.");
-        Console.WriteLine("Вы вошли в систему! Секретные данные: ");
-        Console.WriteLine(File.ReadAllText(secretFile.FullName));
+
+        Console.WriteLine("Вы вошли в систему!");
+        Console.WriteLine("Что вы хотите сделать:");
+        Console.WriteLine("c - сменить пароль");
+        Console.WriteLine("s - доступ к секретной информации");
+        switch (Console.ReadLine())
+        {
+            case "c":
+                change_password(login);
+                break;
+            case "s":
+                Console.WriteLine(return_secret);
+                break;
+        }
     }
     catch(Exception e)
     {
@@ -77,6 +102,19 @@ bool valid_password(string str)
         return true;
     }
     else return false;
+}
+bool valid_new_password(string str)
+{
+    int i = 0;
+    while (i < str.Length)
+    {
+        if (str.IndexOf(str[i], i + 1) != -1)
+        {
+            return false;
+        }
+        i++;
+    }
+    return true;
 }
 bool valid_login(string str)
 {
@@ -105,7 +143,32 @@ string find_login(string login)
     {
         i++;
     }
-    i++;
-    string password = buf.Substring(a, i - a - 1);
+    string password = buf.Substring(a, i - a);
     return password;
+}
+string return_secret()
+{
+    return File.ReadAllText(secretFile.FullName);
+}
+void change_password(string login)
+{
+    
+
+    Console.WriteLine("Введите новый пароль: ");
+    string newPassword= Console.ReadLine();
+    newPassword = newPassword.ToLower();
+    if(!valid_password(newPassword)) throw new Exception("Пароль не правильный, нужна латиница.");
+    if (!valid_new_password(newPassword)) throw new Exception("Пароль не правильный, пароль не должен сожержать повторяющихся символом.");
+
+    string buf = File.ReadAllText(userFile.FullName);
+    int i = buf.IndexOf("login:" + login);
+    i = buf.IndexOf("password:", i);
+    int a = i;
+    while (buf[a] != '\n')
+    {
+        a++;
+    }
+    buf = buf.Remove(i, a - i);
+    buf = buf.Insert(i, "password:" + newPassword);
+    File.WriteAllText(userFile.FullName, buf);
 }
